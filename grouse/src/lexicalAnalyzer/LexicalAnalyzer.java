f64bd5e9bd6b1103767807aa503a1233ca933e8c
alexm <alexm@DESKTOP-K1ON7VV.telus>
2015-10-04 21:35:10 -0700

    Added code for Float.

--- grouse/src/lexicalAnalyzer/LexicalAnalyzer.java
+++ grouse/src/lexicalAnalyzer/LexicalAnalyzer.java
@@ -6,10 +6,11 @@ import inputHandler.LocatedChar;
 import inputHandler.LocatedCharStream;
 import inputHandler.PushbackCharStream;
 import inputHandler.TextLocation;
+import tokens.FloatToken;
 import tokens.IdentifierToken;
 import tokens.LextantToken;
 import tokens.NullToken;
-import tokens.NumberToken;
+import tokens.IntegerToken;
 import tokens.Token;
 import utilities.Debug;
 
@@ -19,7 +20,11 @@ import java.util.Iterator;
 
 public class LexicalAnalyzer extends ScannerImp implements Scanner {
 	private static Debug debug = new Debug();
-	private static boolean commentFlag = false;
+	
+	private enum NumberType {
+		INTEGER,
+		FLOAT;
+	}
 	
 	public static LexicalAnalyzer make(String filename) {
 		InputHandler handler = InputHandler.fromFilename(filename);
@@ -39,34 +44,24 @@ public class LexicalAnalyzer extends ScannerImp implements Scanner {
 		LocatedChar ch = nextNonWhitespaceChar();
 		
 		// beginning of each potential token
-		debug.out("Potential Token spot: " + ch.toString());
+		//debug.out("Potential Token spot: " + ch.toString());
 		
-		if (commentFlag == false) {
-			if(ch.isDigit()) {
-				return scanNumber(ch);
-			}
-			else if(ch.isLowerCase()) {
-				return scanIdentifier(ch);
-			}
-			
-			//else if (isCommentStart(ch)) {
-				// if we find a comment then we remove all tokens until /n or // is found
-				// set flag to true
-				
-			//	return checkIfComment();
-			//}
-			else if(isPunctuatorStart(ch)) {
-				return PunctuatorScanner.scan(ch, input);
-			}
-			else if(isEndOfInput(ch)) {
-				return NullToken.make(ch.getLocation());
-			}
-			else {
-				lexicalError(ch);
-				return findNextToken();
-			}
-		} else {
+		if(ch.isDigit()) {
+			// if we find a digit, keep parsing input until we find a blank space or a period 
+			return scanNumber(ch);
+		}
+		else if(ch.isLowerCase()) {
+			return scanIdentifier(ch);
+		}
+		else if(isPunctuatorStart(ch)) {
+			return PunctuatorScanner.scan(ch, input);
+		}
+		else if(isEndOfInput(ch)) {
 			return NullToken.make(ch.getLocation());
+		}
+		else {
+			lexicalError(ch);
+			return findNextToken();
 		}
 	}
 
@@ -82,24 +77,56 @@ public class LexicalAnalyzer extends ScannerImp implements Scanner {
 	}
 	
 	//////////////////////////////////////////////////////////////////////////////
-	// Integer lexical analysis	
+	// Integer and Float lexical analysis	
 	private Token scanNumber(LocatedChar firstChar) {
 		StringBuffer buffer = new StringBuffer();
+		NumberType numberType;
 		
 		buffer.append(firstChar.getCharacter());
-		appendSubsequentDigits(buffer);
 		
-		return NumberToken.make(firstChar.getLocation(), buffer.toString());
+		numberType = appendSubsequentDigits(buffer);
+		
+		debug.out("scanned number value: " + buffer.toString());
+		
+		if (numberType == NumberType.INTEGER) { // It is an Integer
+			return IntegerToken.make(firstChar.getLocation(), buffer.toString());
+		} else if (numberType == NumberType.FLOAT) {
+			return FloatToken.make(firstChar.getLocation(), buffer.toString());
+		} else {
+			throw new IllegalArgumentException("found : number is not an Integer or a Float");
+		}
 	}
 	
-	private void appendSubsequentDigits(StringBuffer buffer) {
+	// Not actually appending the digits, instead we are looking for the end of the statement
+	private NumberType appendSubsequentDigits(StringBuffer buffer) {
 		LocatedChar c = input.next();
-		
+
+		// Search input until we find a non-digit (ideally a '.' or a ';'
 		while(c.isDigit()) {
 			buffer.append(c.getCharacter());
 			c = input.next();
 		}
-		input.pushback(c);
+		
+		//debug.out("Character: " + c.getCharacter());
+		
+		// If we found a '.', we have found a Float, otherwise it is an Integer
+		if (c.getCharacter() == '.') {
+			buffer.append(c.getCharacter());
+			c = input.next();
+			
+			while(c.isDigit()) {
+				buffer.append(c.getCharacter());
+				c = input.next();
+			}
+			
+			input.pushback(c);
+			
+			return NumberType.FLOAT;
+		} else {
+			input.pushback(c);
+			
+			return NumberType.INTEGER;
+		}
 	}
 	
 	//////////////////////////////////////////////////////////////////////////////
@@ -126,10 +153,12 @@ public class LexicalAnalyzer extends ScannerImp implements Scanner {
 	
 	private void appendSubsequentLowercase(StringBuffer buffer) {
 		LocatedChar c = input.next();
+		
 		while(c.isLowerCase()) {
 			buffer.append(c.getCharacter());
 			c = input.next();
 		}
+		
 		input.pushback(c);
 	}
 
@@ -137,6 +166,7 @@ public class LexicalAnalyzer extends ScannerImp implements Scanner {
 	// Character-classification routines specific to Grouse scanning.	
 	private boolean isPunctuatorStart(LocatedChar lc) {
 		char c = lc.getCharacter();
+		
 		return isPunctuatorStartingCharacter(c);
 	}
 
@@ -144,31 +174,31 @@ public class LexicalAnalyzer extends ScannerImp implements Scanner {
 		return lc == LocatedCharStream.FLAG_END_OF_INPUT;
 	}
 	
-	private boolean isBackSlash(LocatedChar lc) {
-		char c = lc.getCharacter();
-		char nextCharacter = input.peek().getCharacter();
-		
-		debug.out("PEEKED CHAR: " + nextCharacter);
-		
-		// If next token is a "/" then it is a comment, if it is a blank then it is a Punctuator
-		// otherwise, it's an error
-		if (nextCharacter == '/') {
-			// Then we have a Comment
-			
-		} else if (nextCharacter == ' ') {
-			// Then we have a Punctuator
-			
-		} else {
-			// Throw an error
-			
-		}
-		
-		return isCommentStartingCharacter(c);
-	}
+	// unused
+//	private boolean isBackSlash(LocatedChar lc) {
+//		char c = lc.getCharacter();
+//		char nextCharacter = input.peek().getCharacter();
+//		
+//		debug.out("PEEKED CHAR: " + nextCharacter);
+//		
+//		// If next token is a "/" then it is a comment, if it is a blank then it is a Punctuator
+//		// otherwise, it's an error
+//		if (nextCharacter == '/') {
+//			// Then we have a Comment
+//			
+//		} else if (nextCharacter == ' ') {
+//			// Then we have a Punctuator
+//			
+//		} else {
+//			// Throw an error
+//			
+//		}
+//		
+//		return isCommentStartingCharacter(c);
+//	}
 	
+	// unused
 	private boolean isCommentStartingCharacter(char c) {
-		
-		
 		return true;
 	}
 	
