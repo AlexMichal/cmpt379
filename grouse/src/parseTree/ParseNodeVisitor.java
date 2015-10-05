2789cfaf0f24613cdbe4c11a1b6ef2bcee67ce76
alexm <alexm@DESKTOP-K1ON7VV.telus>
2015-10-05 00:17:44 -0700

    Added code for Floating type.

--- grouse/src/parseTree/ParseNodeVisitor.java
+++ grouse/src/parseTree/ParseNodeVisitor.java
@@ -7,6 +7,7 @@ import parseTree.nodeTypes.DeclarationNode;
 import parseTree.nodeTypes.ErrorNode;
 import parseTree.nodeTypes.IdentifierNode;
 import parseTree.nodeTypes.IntegerConstantNode;
+import parseTree.nodeTypes.FloatConstantNode;
 import parseTree.nodeTypes.NewlineNode;
 import parseTree.nodeTypes.PrintStatementNode;
 import parseTree.nodeTypes.ProgramNode;
@@ -41,6 +42,7 @@ public interface ParseNodeVisitor {
 	void visit(ErrorNode node);
 	void visit(IdentifierNode node);
 	void visit(IntegerConstantNode node);
+	void visit(FloatConstantNode node);
 	void visit(NewlineNode node);
 	void visit(SeparatorNode node);
 
@@ -106,6 +108,9 @@ public interface ParseNodeVisitor {
 			defaultVisitForLeaf(node);
 		}
 		public void visit(IntegerConstantNode node) {
+			defaultVisitForLeaf(node);
+		}
+		public void visit(FloatConstantNode node) {
 			defaultVisitForLeaf(node);
 		}
 		public void visit(NewlineNode node) {
