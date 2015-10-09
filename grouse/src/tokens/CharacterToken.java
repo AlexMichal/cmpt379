f8929c8057b49b55e57b9c74f0b0b49d4d80b2e9
amichal <amichal@CS-SC4050-D01.cs.surrey.sfu.ca>
2015-10-09 11:40:29 -0700

    Finalized Character token.

--- grouse/src/tokens/CharacterToken.java
+++ grouse/src/tokens/CharacterToken.java
@@ -5,7 +5,6 @@ import utilities.Debug;
 
 public class CharacterToken extends TokenImp {
 	protected char value;
-	private static Debug debug = new Debug(); // TODO: REMOVE AFTER DONE TESTING
 	
 	protected CharacterToken(TextLocation location, String lexeme) {
 		super(location, lexeme);
@@ -19,12 +18,8 @@ public class CharacterToken extends TokenImp {
 	
 	public static CharacterToken make(TextLocation location, String lexeme) {
 		CharacterToken result = new CharacterToken(location, lexeme);
-		
-		debug.out("Character token: " + lexeme.charAt(0));
-		
-		result.setValue(lexeme.charAt(0)); // TODO: character related HERE
-		
-		debug.out("Character token: " + result.getValue());
+
+		result.setValue(lexeme.charAt(0));
 		
 		return result;
 	}
