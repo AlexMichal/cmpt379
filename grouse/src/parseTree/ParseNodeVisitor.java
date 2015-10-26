3a055e89a55c9158cfba0eed20ad73451513658b
alexm <alexm@DESKTOP-K1ON7VV.telus>
2015-10-26 14:58:44 -0700

    Fixed comments and indentation.

--- grouse/src/parseTree/ParseNodeVisitor.java
+++ grouse/src/parseTree/ParseNodeVisitor.java
@@ -3,10 +3,11 @@ package parseTree;
 import parseTree.nodeTypes.BinaryOperatorNode;
 import parseTree.nodeTypes.BooleanConstantNode;
 import parseTree.nodeTypes.CharacterConstantNode;
-import parseTree.nodeTypes.MainBlockNode;
+import parseTree.nodeTypes.BlockNode;
 import parseTree.nodeTypes.DeclarationNode;
 import parseTree.nodeTypes.ErrorNode;
 import parseTree.nodeTypes.IdentifierNode;
+import parseTree.nodeTypes.IfStatementNode;
 import parseTree.nodeTypes.IntegerConstantNode;
 import parseTree.nodeTypes.LetStatementNode;
 import parseTree.nodeTypes.FloatConstantNode;
@@ -19,33 +20,35 @@ import parseTree.nodeTypes.UnaryOperatorNode;
 
 // Visitor pattern with pre- and post-order visits
 public interface ParseNodeVisitor {
-	
-	// non-leaf nodes: visitEnter and visitLeave
+	// NON-LEAF NODES: visitEnter and visitLeave
 	void visitEnter(BinaryOperatorNode node);
 	void visitLeave(BinaryOperatorNode node);
 	
 	void visitEnter(UnaryOperatorNode node);
 	void visitLeave(UnaryOperatorNode node);
 	
-	void visitEnter(MainBlockNode node);
-	void visitLeave(MainBlockNode node);
-
+	void visitEnter(BlockNode node);
+	void visitLeave(BlockNode node);
+	
+	void visitEnter(PrintStatementNode node);
+	void visitLeave(PrintStatementNode node);
+	
 	void visitEnter(DeclarationNode node);
 	void visitLeave(DeclarationNode node);
 	
 	void visitEnter(LetStatementNode node);
 	void visitLeave(LetStatementNode node);
 	
+	void visitEnter(IfStatementNode node);
+	void visitLeave(IfStatementNode node);
+	
 	void visitEnter(ParseNode node);
 	void visitLeave(ParseNode node);
-	
-	void visitEnter(PrintStatementNode node);
-	void visitLeave(PrintStatementNode node);
 	
 	void visitEnter(ProgramNode node);
 	void visitLeave(ProgramNode node);
 
-	// leaf nodes: visitLeaf only
+	// LEAF NODES: visitLeaf only
 	void visit(BooleanConstantNode node);
 	void visit(ErrorNode node);
 	void visit(IdentifierNode node);
@@ -55,11 +58,11 @@ public interface ParseNodeVisitor {
 	void visit(StringConstantNode node);
 	void visit(NewlineNode node);
 	void visit(SeparatorNode node);
-
 	
 	public static class Default implements ParseNodeVisitor
 	{
-		public void defaultVisit(ParseNode node) {	}
+		public void defaultVisit(ParseNode node) {	
+		}
 		public void defaultVisitEnter(ParseNode node) {
 			defaultVisit(node);
 		}
@@ -69,6 +72,11 @@ public interface ParseNodeVisitor {
 		public void defaultVisitForLeaf(ParseNode node) {
 			defaultVisit(node);
 		}
+		
+		/*********************************************/
+		/* NON-LEAF NODES: visitEnter and visitLeave */
+		/*********************************************/
+		
 		public void visitEnter(BinaryOperatorNode node) {
 			defaultVisitEnter(node);
 		}
@@ -93,10 +101,16 @@ public interface ParseNodeVisitor {
 		public void visitLeave(LetStatementNode node) {
 			defaultVisitLeave(node);
 		}	
-		public void visitEnter(MainBlockNode node) {
+		public void visitEnter(IfStatementNode node) {
 			defaultVisitEnter(node);
 		}
-		public void visitLeave(MainBlockNode node) {
+		public void visitLeave(IfStatementNode node) {
+			defaultVisitLeave(node);
+		}	
+		public void visitEnter(BlockNode node) {
+			defaultVisitEnter(node);
+		}
+		public void visitLeave(BlockNode node) {
 			defaultVisitLeave(node);
 		}				
 		public void visitEnter(ParseNode node) {
@@ -118,7 +132,10 @@ public interface ParseNodeVisitor {
 			defaultVisitLeave(node);
 		}
 		
-
+		/******************************/
+		/* LEAF NODES: visitLeaf only */
+		/******************************/
+		
 		public void visit(BooleanConstantNode node) {
 			defaultVisitForLeaf(node);
 		}
