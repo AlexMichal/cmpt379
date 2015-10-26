bb5942a588ae753a31e065a935f3190ca643d347
alexm <alexm@DESKTOP-K1ON7VV.telus>
2015-10-26 14:17:16 -0700

    Updated if statement node with overloaded function for else statements.

--- grouse/src/parseTree/nodeTypes/IfStatementNode.java
+++ grouse/src/parseTree/nodeTypes/IfStatementNode.java
@@ -3,11 +3,14 @@ package parseTree.nodeTypes;
 import parseTree.ParseNode;
 import parseTree.ParseNodeVisitor;
 import lexicalAnalyzer.Keyword;
+import lexicalAnalyzer.Lextant;
+import tokens.LextantToken;
 import tokens.Token;
 
 public class IfStatementNode extends ParseNode {
 	public IfStatementNode(Token token) {
 		super(token);
+		
 		assert(token.isLextant(Keyword.IF));
 	}
 
@@ -17,7 +20,35 @@ public class IfStatementNode extends ParseNode {
 
 	////////////////////////////////////////////////////////////
 	// attributes
+	public Lextant getIfStatementType() {
+		return lextantToken().getLextant();
+	}
 	
+	public LextantToken lextantToken() {
+		return (LextantToken)token;
+	}
+	
+	////////////////////////////////////////////////////////////
+	// convenience factory
+	public static IfStatementNode withChildren(Token token, ParseNode expression, ParseNode block) {
+		IfStatementNode node = new IfStatementNode(token);
+		
+		node.appendChild(expression);
+		node.appendChild(block);
+		
+		return node;
+	}
+	
+	// With an ELSE statement
+	public static IfStatementNode withChildren(Token token, ParseNode expression, ParseNode ifStatementBlock, ParseNode elseStatementBlock) {
+		IfStatementNode node = new IfStatementNode(token);
+		
+		node.appendChild(expression);
+		node.appendChild(ifStatementBlock);
+		node.appendChild(elseStatementBlock);
+		
+		return node;
+	}
 	
 	///////////////////////////////////////////////////////////
 	// boilerplate for visitors
